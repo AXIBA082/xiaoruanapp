9a7075ba52dd37ba17d3d576774e9513b65c4313
AXIBA082 <89534421+AXIBA082@users.noreply.github.com>
2024-01-14 03:41:39 +0800

    评论v2

---
+++ src/main/java/com/example/xiaoruan/entity/Comment.java
@@ -0,0 +1,9 @@
+package com.example.xiaoruan.entity;
+
+import lombok.Data;
+
+@Data
+public class Comment {
+    private String nickname;
+    private String content;
+}
--- src/main/java/com/example/xiaoruan/service/impl/CommentsServiceImpl.java
+++ src/main/java/com/example/xiaoruan/service/impl/CommentsServiceImpl.java
@@ -1,32 +1,47 @@
 package com.example.xiaoruan.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.xiaoruan.entity.Comment;
 import com.example.xiaoruan.entity.Comments;
 import com.example.xiaoruan.entity.Posts;
+import com.example.xiaoruan.entity.User;
 import com.example.xiaoruan.repository.CommentsRepository;
 import com.example.xiaoruan.repository.PostsRepository;
+import com.example.xiaoruan.repository.UserRepository;
 import com.example.xiaoruan.service.CommentsService;
 import com.example.xiaoruan.vo.ResultVO;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Service("commentsService")
 public class CommentsServiceImpl  implements CommentsService {
     @Resource
-
     private CommentsRepository commentsRepository;
+
+    @Resource
+    private UserRepository userRepository;
 
     @Override
     public ResultVO getcomments(int postId){
         ResultVO resultVO = new ResultVO();
         QueryWrapper<Comments> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("postid",postId);
+        List<Comment> commentList=new ArrayList<>();
         List<Comments> commentsList=commentsRepository.selectList(queryWrapper);
+        for(Comments a:commentsList){
+            QueryWrapper<User> queryWrapper1 = new QueryWrapper<>();
+            Comment temp = new Comment();
+            queryWrapper1.eq("id",a.getUserid());
+            temp.setNickname(userRepository.selectOne(queryWrapper1).getNickname());
+            temp.setContent(a.getContent());
+            commentList.add(temp);
+        }
         resultVO.setCode(0);
-        resultVO.setData(commentsList);
+        resultVO.setData(commentList);
         return resultVO;
     }
 }
